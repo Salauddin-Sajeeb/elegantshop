e0dd90b3eade37aa2b94fd038274e29dbfea3fe6
Salauddin Sajeebe <salauddin15-749@diu.edu.bd>
2025-08-28 03:06:20 +0600

    server ith js

--- server/storage.ts
+++ server/storage.ts
@@ -16,8 +16,8 @@ import {
   type InsertOrder,
   orders, orderStatusEnum
 
-} from "./shared/schema";
-import { db } from "./db";
+} from "./shared/schema.js";
+import { db } from "./db.js";
 import { eq, sql, and,desc } from "drizzle-orm"; //desc for ordering
 
 import { randomUUID } from "crypto";
